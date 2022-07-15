af4fcfc89712cac9f0921e1f1e047fd46bb31b4a
Florian Wientzek <florian.wientzek@gmx.net>
2022-07-15 21:58:35 +0200

    Local adjustments for Linux, which shows the correct device ideas instead of the Mac implementation...

--- src/thermosrv_blomdoft/main.py
+++ src/thermosrv_blomdoft/main.py
@@ -20,7 +20,7 @@ def detection_callback(device, advertisement_data):
 
     if advertisement_data.local_name is not None and advertisement_data.local_name.startswith("ATC"):
         data_str = list(advertisement_data.service_data.values())[0]
-        name = "ATC_" + device.address[-6:]
+        name = "ATC_" + device.address.replace(":", "")[-6:]
         temp = int.from_bytes(data_str[6:8], "big", signed=True) / 10
         hum = data_str[8]
         Measures().add_measure(name, temp, hum)
